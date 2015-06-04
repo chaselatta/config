d5f7a41afef56f7e63272b6157c668e3da3033c7
Chase Latta <clatta@twitter.com>
2015-06-04 15:18:36 -0700

    Renaming some commands

--- fish/git.fish
+++ fish/git.fish
@@ -1,5 +1,5 @@
-alias gbr 'git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
-alias grmd "git ls-files -d -z | xargs -0 git rm"
+alias git-rm-merged 'git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+alias git-rm-files "git ls-files -d -z | xargs -0 git rm"
 alias last-commit "git log --pretty=oneline -n1 | cut -c 1-6 | tr '[a-z]' '[A-Z]'"
 alias gp 'git pull'
 alias gcommit 'git commit -m'
@@ -7,10 +7,11 @@ alias gst 'git status'
 alias glg 'git lg'
 
 function ghelp
-    echo "* gbr     deletes all merged branches. Should be called from master branch."
-    echo "* gp      git pull"
-    echo "* grmd    removes all deleted files"
-    echo "* gcommit commits with a required message"
-    echo "* gs      git status"
-    echo "* glg     git lg"
+    echo "* git-rm-merged deletes all merged branches. Should be called from master branch"
+    echo "* git-rm-file   removes all deleted files"
+    echo "* last-commit   list the sha of the last commit"
+    echo "* gp            git pull"
+    echo "* gcommit       commits with a required message"
+    echo "* gst           git status"
+    echo "* glg           git lg"
 end
