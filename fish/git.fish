ce3fd81134ded70e25016e1533bcf597f81c5266
Chase Latta <clatta@twitter.com>
2015-06-12 10:25:21 -0700

    Added the git-copy-br alias

--- fish/git.fish
+++ fish/git.fish
@@ -1,5 +1,6 @@
 alias git-rm-merged 'git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 alias git-rm-files "git ls-files -d -z | xargs -0 git rm"
+alias git-copy-br 'git branch | grep "\*" | cut -c 3- | xargs echo -n | pbcopy'
 alias last-commit "git log --pretty=oneline -n1 | cut -c 1-6 | tr '[a-z]' '[A-Z]'"
 alias gp 'git pull'
 alias gcommit 'git commit -m'
@@ -9,6 +10,7 @@ alias glg 'git lg'
 function ghelp
     echo "* git-rm-merged deletes all merged branches. Should be called from master branch"
     echo "* git-rm-file   removes all deleted files"
+    echo "* git-copy-br   copy the current branch to the clipboard"
     echo "* last-commit   list the sha of the last commit"
     echo "* gp            git pull"
     echo "* gcommit       commits with a required message"
